aa7625e9037f1f108e40d4e9783fafb38356c30b
Gifty-Nsowah <nsowahgifty2@gmail.com>
2023-08-11 11:01:32 -0700

    6-size.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+/*
+*A program that prints a line using the printf functions
+*/
+#include <stdio.h>
+int main(void)
+{
+printf("with proper gramar, but the outcome is a place of art,\n")
+return (0);
+}
